9591b525e8a1ce9be46b9f7e2a348295bb684c86
Bruno <bruno.eng.comp@gmail.com>
2017-06-22 00:30:47 -0300

    Adicionar arquivo sb2-admin.js para carregamento dos javascripts necessários

---
+++ grails-app/assets/javascripts/sb2-admin.js
@@ -0,0 +1,8 @@
+//= require jquery/jquery
+//= require bootstrap/js/bootstrap
+//= require metisMenu/metisMenu
+//= require raphael/raphael
+//= require morrisjs/morris
+//= require data/morris-data.js
+
+//= require sb2-admin/sb-admin-2
